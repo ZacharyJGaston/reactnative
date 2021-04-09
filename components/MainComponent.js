8456903adc9d86718172f5097892b6389c1ed76b
ZacharyJGaston <77356785+ZacharyJGaston@users.noreply.github.com>
2021-04-09 12:05:40 -0500

    Add files via upload
    "Navigation Part 3"

---
+++ components/MainComponent.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import Home from './HomeComponent';
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createAppContainer } from 'react-navigation-stack';
+
+const DirectoryNavigator = createStackNavigator(
+  {
+      Directory: { screen: Directory },
+      CampsiteInfo: { screen: CampsiteInfo }
+  }, 
+  {
+      initialRouteName: 'Directory',
+      defaultNavigationOptions: {
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          }
+      }
+  }
+);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);    
+
+const AppNavigator = createAppContainer(MainNavigator);
+
+class Main extends Component {
+  render() {
+      return (
+          <View
+              style={{
+                  flex: 1,
+                  paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+          }}>
+              <AppNavigator />
+          </View>
+      );
+  }
+}
+
+export default Main;
