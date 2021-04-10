99e26dbd33dc52ff68e97d7984e4cb690f7e174e
ZacharyJGaston <zgneworleans@aol.com>
2021-04-10 11:06:17 -0500

    files

---
+++ components/DirectoryComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../../shared/campsites';
+
+class Directory extends Component {
+
+        constructor(props) {
+            super(props);
+            this.state = {
+                campsites: CAMPSITES
+            };
+        }
+    
+        static navigationOptions = {
+            title: 'Directory'
+        }
+                
+        render() {
+            const { navigate } = this.props.navigation;
+            const renderDirectoryItem = ({item}) => {
+                return (
+                    <ListItem
+                        title={item.name}
+                        subtitle={item.description}
+                        onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                        leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                    />
+                );
+            };
+
+            return (
+                <FlatList
+                    data={this.state.campsites}
+                    renderItem={renderDirectoryItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            );
+        }
+    }
+
+export default Directory;
