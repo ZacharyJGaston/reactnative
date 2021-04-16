4d7b2691241a50e5ece672b94fd5b37237627a74
ZacharyJGaston <zgneworleans@aol.com>
2021-04-15 22:37:06 -0500

    Using Redux in React Native

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,6 +10,16 @@ import { createDrawerNavigato, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions,
+    fetchPartners } from '../redux/ActionCreators';
+
+    const mapDispatchToProps = {
+      fetchCampsites,
+      fetchComments,
+      fetchPromotions,
+      fetchPartners
+  };
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -185,6 +195,14 @@ const MainNavigator = createDrawerNavigator(
 );
 const AppNavigator = createAppContainer(MainNavigator);
 class Main extends Component {
+
+  componentDidMount() {
+    this.props.fetchCampsites();
+    this.props.fetchComments();
+    this.props.fetchPromotions();
+    this.props.fetchPartners();
+}
+
   render() {
     return (
       <View
@@ -229,4 +247,4 @@ drawerImage: {
   }
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
